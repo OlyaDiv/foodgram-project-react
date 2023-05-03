8ba893f44e903a94dcd1830ec789f68d644b8fed
Ольга Дивногорская <olga.divnogorsckaya@yandex.ru>
2023-05-03 15:06:40 +0300

    ordering

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -105,7 +105,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
-        ordering = ['name']
+        ordering = ['-id']
 
 
 class IngredientRecipe(models.Model):
